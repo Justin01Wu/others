006a6e7b8b086e4d1ac594d9285eefd313791448
Justin01Wu <justin01.wu@gmail.com>
2018-12-19 16:18:29 -0500

    improve docker samples

--- docker/ranImages.bat
+++ docker/ranImages.bat
@@ -5,7 +5,9 @@ docker run demo/openjdk:8
 rem run and open it
 rem docker run -it demo/openjdk:8 sh
 
-rem docker run -p 2180:2180 -it dev/kafka:2.1.0 bash
-rem docker run -p 8080:8080 -it dev/tomcat8.5.35 bash
+rem docker run -p 2180:2180 -p 9092:9092 -it dev/kafka:2.1.0 bash
+
+rem expose inner tomcat 8080 to host 8686 
+rem docker run -p 8686:8080 -it dev/tomcat8.5.35 bash
 
 
