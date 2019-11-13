82f6af4075c61aed1fcf70de1a177331821ecc8f
Justin01wu <Justin01.wu@gmail.com>
2019-11-12 22:21:04 -0500

    add python aws sample

---
+++ python/aws/helloWorld.py
@@ -0,0 +1,21 @@
+import boto3
+
+#Explicit Client Configuration
+
+polly = boto3.client('polly',
+	region_name='us-east-1',
+	aws_access_key_id='AKIAR74OGT4XIRTK2KQO',
+	aws_secret_access_key='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'
+)
+mySentence = ('Please log in to start creating quizzes to practice and study. '
+	'If you haven’t yet created an account, follow the Create Account link to get started.'
+	' Once you have an account created and are logged in, '
+	'you’ll be able to redeem your Access Code or PIN to add products to your account.')
+result = polly.synthesize_speech(Text=mySentence,
+	OutputFormat='mp3',
+	VoiceId='Aditi')
+
+# Save the Audio from the response
+audio = result['AudioStream'].read()
+with open("helloworld.mp3","wb") as file:
+	file.write(audio)
