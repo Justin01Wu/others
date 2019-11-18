fdd8098b3762b149cdc8db74f00f05fc787a9011
Justin01wu <Justin01.wu@gmail.com>
2019-11-17 22:30:32 -0500

    add sample createTomcatEc2

---
+++ aws/cli/createTomcatEc2/createEC2.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+
+rem  create EC2 instance from CLI:
+
+set para1=--image-id ami-00dc79254d0461090 --instance-type t2.micro
+set para2=--iam-instance-profile Name=WebServerRole --key-name MyUser2KP --security-groups WebDMZ
+set para3=--user-data file://install_tomcat8.5.txt
+
+aws ec2 run-instances %para1%  %para2% %para3%
