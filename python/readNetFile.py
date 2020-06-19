77566d30a5284a6c5ec612552ab3f45081e90984
Justin01Wu <justin01.wu@gmail.com>
2020-06-19 11:09:40 -0400

    improve python/readNetFile.py

--- python/readNetFile.py
+++ python/readNetFile.py
@@ -2,4 +2,11 @@ from urllib.request import urlopen
 
 url = urlopen("http://www.justa999.com/readNetFile.py")
 for line in url:
-    print(line )
+    print(line.strip() )
+
+#fileName = "F:\projects\python_test\cemc\pathfinder2.py"
+fileName = "//f2/Volume_1/readme.txt"
+f = open(fileName, "r")
+Lines = f.readlines()
+for line in Lines: 
+    print(line.strip()) 
